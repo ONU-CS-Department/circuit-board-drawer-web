51fb864966528e042d13658a8c9d88d8651f10ee
Nathan Downey <npdowney97@gmail.com>
2018-10-05 11:37:59 -0500

    Check boxes do something

--- line_graphic.js
+++ line_graphic.js
@@ -4,19 +4,59 @@ var ctx = canvas.getContext("2d");
 var prevX = 0;
 var prevY = 0;
 
+
+
+function drawingFunction() {
+    var hasStartNode = document.getElementById("startNode");
+    var hasEndNode = document.getElementById("endNode");
+    var hasStartModule = document.getElementById("startModule");
+    var hasEndModule = document.getElementById("endModule");
+    var isContinuous = document.getElementById("continuous");
+    var text = document.getElementById("text");
+
+    if (hasStartNode.checked == true){
+        text.style.display = "block";
+        console.log("start Node");
+    } else {
+       text.style.display = "none";
+    }
+
+    if (hasEndModule.checked == true) {
+        console.log("End Module");
+    }
+    if (hasEndNode.checked == true) {
+        console.log("hasEndNode");
+    }
+    if (hasStartModule.checked == true) {
+        console.log("hasStartModule");
+    }
+    if (hasEndModule.checked == true) {
+        console.log("hasEndModule");
+    }
+    if (isContinuous.checked == true) {
+        text.style.display = "block";
+        console.log("its working");
+        canvas.addEventListener("click", function (evt) {
+            var mousePos = getMousePos(canvas, evt);
+             ctx.beginPath();
+             ctx.lineWidth = "8";
+             ctx.moveTo(prevX,prevY);
+             ctx.lineTo(mousePos.x,mousePos.y);
+             ctx.strokeStyle="#FCFDFD";
+             ctx.stroke();
+            prevX = mousePos.x;
+            prevY = mousePos.y;
+        }, false);
+    }
+    else {
+        text.style.display = "none";
+    }
+}
+
+
+
 //report the mouse position on click
-canvas.addEventListener("click", function (evt) {
-    var mousePos = getMousePos(canvas, evt);
-    //alert(mousePos.x + ',' + mousePos.y);
-     ctx.beginPath();
-     ctx.lineWidth = "8";
-     ctx.moveTo(prevX,prevY);
-     ctx.lineTo(mousePos.x,mousePos.y);
-     ctx.strokeStyle="#FCFDFD";
-     ctx.stroke();
-    prevX = mousePos.x;
-    prevY = mousePos.y;
-}, false);
+
 
 //Get Mouse Position
 function getMousePos(canvas, evt) {
@@ -26,22 +66,3 @@ function getMousePos(canvas, evt) {
         y: evt.clientY - rect.top
     };
 }
-
-
-//My code
-// var canvasSize = canvas.getBoundingClientRect();
-
-// canvas.addEventListener("click", clickFunction); 
-
-// var newX = 0;
-// var newY = 0;
-
-
-
-// function clickFunction() {
-
-
-
-//     prevX = newX;
-//     prevY = newY;
-// }
